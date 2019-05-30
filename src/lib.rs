89a4599b95dd30bdbf9def4ee7d5137a53925fd2
Siddharth Yadav <me@sidyadav.dev>
2019-05-30 14:39:35 +0530

    working cron server without config and jobsfile parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,69 @@
+#[macro_use]
+extern crate log;
+
+use cron::Schedule;
+use chrono::Local;
+use chrono::DateTime;
 use env_logger::{Builder, Target};
 use log::{error, info};
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd::{execv, fork, getpid, ForkResult, Pid};
 use std::cmp::Ordering;
 use std::ffi::CString;
+use std::str::FromStr;
 use std::thread;
 use std::time;
 
-#[derive(Debug, Eq, PartialEq, Clone)]
-pub struct JobSchedule {}
-
-#[derive(Debug, Eq, PartialEq, Clone)]
+#[derive(Eq, PartialEq, Clone)]
 struct Job {
-    prev: time::SystemTime,
+    name: String,
+    prev: DateTime<Local>,
     cmd: String,
     params: Vec<CString>,
-    next: time::SystemTime,
+    schedule: Schedule,
+    expression: String,
+    next: DateTime<Local>,
 }
 
 impl Job {
-    pub fn new(cmd: String, next: time::SystemTime) -> Self {
+    pub fn new(name: String, cmd: String, expr: &str) -> Self {
         // Build params
         let mut p: Vec<CString> = vec![];
         for a in cmd.split(' ') {
             p.push(CString::new(a).unwrap());
         }
 
+        let schedule = Schedule::from_str(expr).unwrap();
+        let next = schedule.upcoming(Local).next().unwrap();
+
         Job {
+            name,
             cmd,
             next,
-            prev: time::SystemTime::now(),
+            expression: expr.to_string(),
+            schedule: schedule,
+            prev: Local::now(),
             params: p,
         }
     }
 }
 
-#[derive(Debug, Eq, Clone)]
+impl std::fmt::Debug for Job {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Job: {} ({})", self.name, self.next)
+    }
+}
+
+#[derive(Eq, Clone)]
 struct Event {
-    time: time::SystemTime,
-    pub jobs: Vec<Job>,
+    time: DateTime<Local>,
+    jobs: Vec<Job>,
+}
+
+impl std::fmt::Debug for Event {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Time: {} -> Jobs: {:?}", self.time, self.jobs)
+    }
 }
 
 impl Ord for Event {
@@ -60,15 +85,18 @@ impl PartialEq for Event {
 }
 
 impl Event {
-    pub fn new(t: time::SystemTime) -> Self {
+    pub fn new(t: DateTime<Local>) -> Self {
         Event {
             time: t,
             jobs: vec![],
         }
     }
+
+    pub fn push_job(&mut self, j: Job) {
+        self.jobs.push(j)
+    }
 }
 
-#[derive(Debug)]
 struct EventQueue {
     queue: Vec<Event>,
 }
@@ -95,12 +123,12 @@ impl EventQueue {
             match self.queue.binary_search_by(|probe| j.next.cmp(&probe.time)) {
                 Ok(pos) => {
                     // Already in the vector
-                    self.queue[pos].jobs.push(j);
+                    self.queue[pos].push_job(j);
                 }
                 Err(pos) => {
                     // Not in the vector
                     let mut e = Event::new(j.next);
-                    e.jobs.push(j);
+                    e.push_job(j);
                     self.queue.insert(pos, e);
                 }
             }
@@ -111,12 +139,12 @@ impl EventQueue {
         self.queue.pop()
     }
 
-    pub fn top(&self) -> Option<Event> {
-        self.queue.last().cloned()
+    pub fn debug_print(&self) {
+        // print queue for debugging purpose
+        debug!("Queue: {:?}", self.queue);
     }
 }
 
-#[derive(Debug)]
 pub struct Cron {
     job_list: EventQueue,
     wakeup_after: time::Duration,
@@ -137,7 +165,18 @@ impl Cron {
     /// Any configuration related work for cron daemon should be done
     /// in this function.
     pub fn init(&mut self) {
-        // 1. TODO: Read cron schedule files
+        // 1. TODO: Read cron job files
+        // Cron's `Jobsfile` format
+        // - Job 1:
+        //     - cmd: /usr/bin/touch /tmp/1
+        //     - schedule: 0 0/1 * * * *
+        // - Job 2:
+        //     - cmd: /usr/bin/touch /tmp/2
+        //     - schedule: 0 0/2 * * * *
+        // - Job 3:
+        //     - cmd: /usr/bin/touch /tmp/3
+        //     - schedule: 0 0/3 * * * *
+
         // 2. TODO: Parse each file
         // 3. TODO: Enqueue jobs in job_list
 
@@ -146,22 +185,13 @@ impl Cron {
         log_builder.target(Target::Stdout);
         log_builder.init();
 
-        let d = time::Duration::new(30, 0);
-
-        let t1 = time::SystemTime::now() + d;
-        let t2 = t1 + d * 2;
-        let t3 = t2 + d * 4;
-        let t4 = t2;
-
-        let j1 = Job::new("/usr/bin/touch /tmp/1".to_string(), t1);
-        let j2 = Job::new("/usr/bin/touch /tmp/2".to_string(), t2);
-        let j3 = Job::new("/usr/bin/touch /tmp/3".to_string(), t3);
-        let j4 = Job::new("/usr/bin/touch /tmp/4".to_string(), t4);
+        let j1 = Job::new("Job 1".to_string(), "/usr/bin/touch /tmp/1".to_string(), "@minute");
+        let j2 = Job::new("Job 2".to_string(), "/usr/bin/touch /tmp/2".to_string(), "0 0/2 * * * *");
+        let j3 = Job::new("Job 3".to_string(), "/usr/bin/touch /tmp/3".to_string(), "0 0/3 * * * *");
 
         self.job_list.enqueue(j1);
         self.job_list.enqueue(j2);
         self.job_list.enqueue(j3);
-        self.job_list.enqueue(j4);
     }
 
     /// This starts the actual cron server
@@ -170,8 +200,10 @@ impl Cron {
         self.zombie_reaper();
 
         loop {
+            self.job_list.debug_print();
+
             // Check if there is any thing in the queue
-            let top = match self.job_list.top() {
+            let top = match self.job_list.dequeue() {
                 Some(t) => t,
                 None => {
                     // if queue is empty, sleep for a minute and try again
@@ -181,27 +213,22 @@ impl Cron {
             };
 
             // 1. Calculate wakeup after
-            self.wakeup_after = top
-                .time
-                .duration_since(time::SystemTime::now())
-                .expect("sleep time calculation failed");
-            self.wakeup_after = time::Duration::new(self.wakeup_after.as_secs(), 0);
+            let wakeup_after = match top.time.signed_duration_since(Local::now()).to_std() {
+                Ok(t) => t,
+                Err(err) => {
+                    error!("Failed to calculate time difference for time {}: {}", top.time, err);
+                    thread::sleep(time::Duration::from_secs(60));
+                    continue;
+                }
+            };
+            self.wakeup_after = time::Duration::new(wakeup_after.as_secs(), 0);
 
             info!("Next exec after time {:?}", self.wakeup_after);
 
             // 2. sleep for wakeup_after duration
             thread::sleep(self.wakeup_after);
 
-            // 3. dequeue element from queue
-            let e = match self.job_list.dequeue() {
-                Some(e) => e,
-                None => {
-                    error!("Failed to dequeue element");
-                    continue;
-                }
-            };
-
-            for j in e.jobs {
+            for j in top.jobs {
                 // 4. fork process
                 match fork() {
                     Ok(ForkResult::Child) => {
@@ -210,7 +237,7 @@ impl Cron {
                         // 5. execve job on forked process
                         match execv(path, &j.params[..]) {
                             Ok(_) => {
-                                info!("Ran job {} in process {}", j.cmd, getpid());
+                                info!("Ran job {} in process {}", j.name, getpid());
                             }
                             Err(err) => {
                                 error!("Failed to execute `{:?}` in pid `{}`: {:?}", path, getpid(), err);
@@ -218,14 +245,21 @@ impl Cron {
                         }
                     }
                     Ok(ForkResult::Parent {child}) => {
-                        info!("Spawned child {}", child);
+                        info!("Spawned child {} for job {}", child, j.name);
 
-                        let mut j_new = Job::new(j.cmd, j.next + time::Duration::new(120, 0));
+                        if !j.schedule.upcoming(Local).peekable().peek().is_some() {
+                            info!("Job Schedule Finished: {:?}", j.name);
+                            continue;
+                        }
+
+                        // Requeue /w new `next`
+                        let mut j_new = j.clone();
                         j_new.prev = j.next;
+                        // In theory this unwrap should not fail because we peek into the iterator above
+                        // and if it's empty we continue the loop without requeueing
+                        j_new.next = j.schedule.after(&DateTime::from(time::SystemTime::now() + time::Duration::from_secs(1))).next().unwrap();
+                        debug!("New Job: {:?}", j_new);
                         self.job_list.enqueue(j_new);
-
-                        // 6. goto 1
-                        continue;
                     }
                     Err(_) => error!("Forking should never fail!!!.
                     If you are seeing this message, then you have much more serious problems than this server failing."),
@@ -240,23 +274,23 @@ impl Cron {
             match waitpid(Pid::from_raw(-1), Some(WaitPidFlag::WNOHANG)) {
                 Ok(s) => match s {
                     WaitStatus::Exited(pid, code) => {
-                        info!("Process {} exited with code {}", pid, code)
+                        info!("[Reaper] Process {} exited with code {}", pid, code)
                     }
                     WaitStatus::Stopped(pid, signal) => {
-                        info!("Process {} stopped by signal {:?}", pid, signal)
+                        info!("[Reaper] Process {} stopped by signal {:?}", pid, signal)
                     }
                     WaitStatus::Signaled(pid, signal, _) => {
-                        info!("Process {} signaled to stop with {:?}", pid, signal)
+                        info!("[Reaper] Process {} signaled to stop with {:?}", pid, signal)
                     }
                     _ => {
-                        info!("Wait Signal: {:?}", s);
-                        thread::sleep(time::Duration::from_secs(10));
+                        info!("[Reaper] Wait Signal: {:?}", s);
+                        thread::sleep(time::Duration::from_secs(60));
                         continue;
                     }
                 },
                 Err(e) => {
-                    info!("Not childs present: {:?}", e);
-                    thread::sleep(time::Duration::from_secs(10));
+                    info!("[Reaper] No childs present: {:?}", e);
+                    thread::sleep(time::Duration::from_secs(60));
                     continue;
                 }
             }
@@ -272,43 +306,5 @@ mod tests {
     /// If this passes, it means the core data structure and it's operations
     /// are performed successfully
     fn enqueue_basic_functionality() {
-        let d = time::Duration::new(120, 0); // 2 minutes
-
-        let t1 = time::SystemTime::now();
-        let t2 = t1 + d;
-        let t3 = t2 + d;
-        let t4 = t2.clone();
-
-        let j1 = Job::new("Job 1".to_string(), t1);
-        let j2 = Job::new("Job 2".to_string(), t2);
-        let j3 = Job::new("Job 3".to_string(), t3);
-        let j4 = Job::new("Job 4".to_string(), t4);
-
-        let mut q = EventQueue::new();
-
-        assert_eq!(q.queue.is_empty(), true);
-
-        // Check enqueue operations
-        q.enqueue(j1);
-        q.enqueue(j2);
-        q.enqueue(j3);
-        q.enqueue(j4);
-
-        // Check Top, Pop operations and ordering of queue
-
-        assert_eq!(q.queue.len(), 3);
-
-        assert_eq!(q.top().unwrap().time, t1);
-        assert_eq!(q.dequeue().unwrap().time, t1);
-
-        assert_eq!(q.top().unwrap().jobs.len(), 2);
-        assert_eq!(q.dequeue().unwrap().time, t2);
-
-        assert_eq!(q.top().unwrap().time, t3);
-        assert_eq!(q.dequeue().unwrap().time, t3);
-
-        assert_eq!(q.top(), Option::None);
-
-        assert_eq!(q.queue.is_empty(), true);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use xcrond::*;
 
 fn main() {
+    // Intitialize signal handler
+    ctrlc::set_handler(move || {
+        println!("Terminate signal received. Exiting.");
+        std::process::exit(0);
+    }).expect("Failed to set SIGINT handler");
+
     let mut c = Cron::new();
     c.init();
     c.run();
